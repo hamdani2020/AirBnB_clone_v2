5c3d05088b53d81df92141226c4b9c04d2fcfd42
hamdani2020 <hamdanialhassangandi2020@gmail.com>
2023-10-21 19:23:52 -0700

    task 9

---
+++ web_flask/9-states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""This script starts a flask application"""
+
+from flask import Flask, render_template
+from models.state import State
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.route('/states')
+@app.route('/states/<id>')
+def states_list(id=None):
+    """This function renders template with states
+    """
+    path = '9-states.html'
+    states = storage.all(State)
+    return render_template(path, states=states, id=id)
+
+
+@app.teardown_appcontext
+def app_teardown(arg=None):
+    """This is a clean-up session
+    """
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
